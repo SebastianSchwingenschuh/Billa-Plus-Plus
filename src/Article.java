b2cbbe87cf27f8f8bace296c3ab6e22512260507
SebastianSchwingenschuh <s.schwingenschuh@students.htl-leonding.ac.at>
2025-10-24 13:20:24 +0200

    Foodarticle Test alles grün

--- src/Article.java
+++ src/Article.java
@@ -6,19 +6,19 @@ public abstract class Article implements Comparable<Article> {
     private int quantity;
 
     public Article(String name, int barcode, int quantity) {
-        if(quantity < 0) {
+        if (quantity < 0) {
             quantity = 0;
         }
         this.name = name;
-        
-        if (!hasValidLastDigit(barcode)) {
+
+        if (!isValidBarcode(barcode)) {
             throw new IllegalArgumentException("Invalid barcode");
         }
         this.barcode = barcode;
         this.quantity = quantity;
     }
-    
-    public boolean hasValidLastDigit(int barcode) {
+
+    public boolean isValidBarcode(int barcode) {
         //Number 4 5 3 4 2 3 2
         //Weight 1 3 1 3 1 3 1
         //Products 4 15 3 12 2 9 2
@@ -26,23 +26,23 @@ public abstract class Article implements Comparable<Article> {
         //Sum mod 10 7
         //Difference to next 10 3
         int lastDigit = barcode % 10;
-        int[] barcodeNumbers = new int[7];
-        for (int i = 6; i >= 0; i--) {
-            barcodeNumbers[i] = barcode % 10;
-            barcode /= 10;
+        int[] barcodeDigits = new int[8];
+
+        for (int i = 7; i >= 0; i--) {
+            barcodeDigits[i] = barcode % 10;
+            barcode = barcode / 10;
         }
+
         int queersum = 0;
         for (int i = 0; i < 7; i++) {
-            if(i % 2 == 1) {
-                queersum += barcodeNumbers[i] * 3;
-                continue;
+            if (i % 2 == 1) {
+                queersum += barcodeDigits[i] * 3;
+            } else {
+                queersum += barcodeDigits[i];
             }
-            queersum += barcodeNumbers[i];
         }
-        if (10 - queersum % 10 == lastDigit) {
-            return true;
-        }
-        return false;
+
+        return 10 - (queersum % 10) == barcodeDigits[7];
     }
 
     public String getName() {
@@ -63,31 +63,31 @@ public abstract class Article implements Comparable<Article> {
         return String.format("#%d %s (%d in stock)", barcode, name, quantity);
     }
 
-    public int buy (int quantity) {
-        if(quantity <= 0) {
+    public int buy(int quantity) {
+        if (quantity <= 0) {
             throw new IllegalArgumentException("Quantity must be greater than 0");
         }
         if (this.quantity < quantity) { //nicht genug im lager
             int quantityBefore = this.quantity;
             //alles aufkaufen
             this.quantity = 0;
-            return quantityBefore;   
+            return quantityBefore;
         }
         this.quantity -= quantity;
         return quantity;
     }
-    
+
     public void restock(int quantity) {
-        if(quantity <= 0) {
+        if (quantity <= 0) {
             throw new IllegalArgumentException("Quantity must be greater than 0");
         }
         this.quantity += quantity;
     }
-    
-    public static boolean checkBarcode (int barcode) {
+
+    public static boolean checkBarcode(int barcode) {
         return false;
     }
-    
+
     public int compareTo(Article article) {
         return this.name.compareTo(article.name);
     }
